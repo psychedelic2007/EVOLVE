27ba09b4fc25995e3686bd408d0b20728c2e692e
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2024-11-23 12:30:37 +1100

    Update transfer_learning.py

--- Models/Transfer Learned Model/transfer_learning.py
+++ Models/Transfer Learned Model/transfer_learning.py
@@ -9,13 +9,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 
-# Custom loss function to avoid log(0)
+# Loss func
 def custom_binary_crossentropy(y_true, y_pred):
     epsilon = 1e-7
     y_pred = tf.clip_by_value(y_pred, epsilon, 1 - epsilon)
     return tf.keras.losses.binary_crossentropy(y_true, y_pred)
 
-# Stating a Function to recreate the model architecture
+#Model architetcure
 def create_model(input_shape):
     inputs = Input(shape=input_shape)
     x = Dense(3, activation='tanh')(inputs)
@@ -30,7 +30,7 @@ pretrained_model = load_model('<pre-trained model name>')
 input_shape = pretrained_model.input_shape[1:]
 print(f"Inferred input shape: {input_shape}")
 
-# Creating new model - keeping the same architecture
+# new model - keeping the same architecture
 base_model = create_model(input_shape)
 
 # Copying weights from pretrained model (Important step!!)
@@ -56,7 +56,6 @@ def load_and_preprocess_data(file_paths):
     X = combined_data.drop('Target', axis=1)
     y = combined_data['Target']
 
-    # Handling NaN values
     imputer = SimpleImputer(strategy='mean')
     X_imputed = imputer.fit_transform(X)
 
