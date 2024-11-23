292316a3d08262c3dfb399a42732f4906408d47a
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2024-11-23 12:29:42 +1100

    Update predictions_using_transfer_learned_model.py

--- Models/Transfer Learned Model/predictions_using_transfer_learned_model.py
+++ Models/Transfer Learned Model/predictions_using_transfer_learned_model.py
@@ -4,27 +4,24 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-# Custom loss function to avoid log(0)
+# Custom loss func
 def custom_binary_crossentropy(y_true, y_pred):
     epsilon = 1e-7
     y_pred = tf.clip_by_value(y_pred, epsilon, 1 - epsilon)
     return tf.keras.losses.binary_crossentropy(y_true, y_pred)
 
-# Loading the model with custom objects
+# MOdle load
 transfer_learned_model = load_model('transfer_learned_model.h5', custom_objects={'custom_binary_crossentropy': custom_binary_crossentropy}, compile=False)
 
-# Function to preprocess and predict
+# preprocess and predict
 def predict_mutation(sequences):
-    # Note: Ensure the scaler is fitted on the training data and used here
     scaler = StandardScaler()
     sequences_scaled = scaler.fit_transform(sequences)
     predictions = transfer_learned_model.predict(sequences_scaled)
     return predictions
 
-# Loading and preprocessing the data
 df = pd.read_csv("test.csv")
 
-# Assuming 'df' contains only features (no labels), preprocess it accordingly
 X_test = df.values
 predictions = predict_mutation(X_test)
 
@@ -35,7 +32,6 @@ plt.plot(predictions1)
 plt.tight_layout()
 plt.show()
 
-# To check the position having higher mutation than the cutoff
 for i in range(len(predictions)):
-    if (predictions1[i] >= 0.2):    #0.2 is the cutoff value. You can change this depending upon how much residue you want above teh cutoff. Suggested value should be varied between 0.2 to 0.6
+    if (predictions1[i] >= 0.2): 
         print("The position is: ", i+1)
