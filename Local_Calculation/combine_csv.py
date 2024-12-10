870df1998c43190bff7f532b045a074397256f91
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2024-12-10 10:16:34 +0530

    Update combine_csv.py

--- Local_Calculation/combine_csv.py
+++ Local_Calculation/combine_csv.py
@@ -2,29 +2,18 @@ import os
 import pandas as pd
 
 def merge_csv_files(folder1, folder2, output_folder):
-    # Ensure the output folder exists
     os.makedirs(output_folder, exist_ok=True)
-    
-    # List all CSV files in the first folder
     files = [f for f in os.listdir(folder1) if f.endswith('.csv')]
     
     for file in files:
         file_path1 = os.path.join(folder1, file)
         file_path2 = os.path.join(folder2, file)
         
-        # Check if the corresponding file exists in the second folder
         if os.path.exists(file_path2):
-            # Read the CSV files
             df1 = pd.read_csv(file_path1)
             df2 = pd.read_csv(file_path2)
-            
-            # Merge the dataframes on the 'Position number' column
             merged_df = pd.merge(df1, df2, on='Position', suffixes=('_F1', '_F2'))
-            
-            # Rename columns appropriately
             merged_df.columns = ['Position', 'F1', 'F2']
-            
-            # Save the merged dataframe to the output folder
             output_file_path = os.path.join(output_folder, file)
             merged_df.to_csv(output_file_path, index=False)
         else:
