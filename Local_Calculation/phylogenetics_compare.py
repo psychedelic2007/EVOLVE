734539bcf4dabf7da4ac30adc0f58241fc295414
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2024-12-10 10:14:29 +0530

    Update phylogenetics_compare.py

--- Local_Calculation/phylogenetics_compare.py
+++ Local_Calculation/phylogenetics_compare.py
@@ -33,11 +33,9 @@ def extract_sequence_from_fasta(fasta_file, accession_id):
 # Main FASTA file containing all sequences
 main_fasta_file = "<name_of_fasta_file_containing_sequences.fasta>"
 
-# Accession IDs of the sequences to be compared
-accession_id1 = "UXX33696.1" #example. This should correspond to accession ID of sequence which you are comparing
-accession_id2 = "UAJ82243.1" #example. This should corerspond to accession ID of sequence with which you are comparing
+accession_id1 = "UXX33696.1" #This should correspond to accession ID of sequence which you are comparing
+accession_id2 = "UAJ82243.1" #This should corerspond to accession ID of sequence with which you are comparing
 
-# Extract sequences from the main FASTA file
 sequence1 = extract_sequence_from_fasta(main_fasta_file, accession_id1)
 sequence2 = extract_sequence_from_fasta(main_fasta_file, accession_id2)
 
@@ -46,10 +44,7 @@ if sequence1 is None:
 elif sequence2 is None:
     print(f"Sequence with Accession ID '{accession_id2}' not found in the main FASTA file.")
 else:
-    # Compare the sequences
     comparison = compare_sequences(sequence1, sequence2)
-
-    # Write the comparison results to a CSV file
     output_csv = f"{accession_id1}_comparison.csv"
     write_comparison_to_csv(comparison, output_csv)
     print(f"Comparison results have been written to '{output_csv}'.")
